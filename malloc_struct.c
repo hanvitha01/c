b2a1479fbfdd1bf3cc575cc204a51b1554ddc50d
Hanvitha Gownipalli <hanvitha.gownipalli@lyptus-tech.com>
2025-12-11 16:05:12 +0530

    struct program 9_12

--- malloc_struct.c
+++ malloc_struct.c
@@ -1,26 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdint.h>
 
-typedef struct db {
-    uint32_t num;
-    struct db *ptr;
-} db_t;
+// typedef struct db {
+//     uint32_t num;
+//     struct db *ptr;
+// } db_t;
 
-int main() {
-    db_t *p = malloc(sizeof(db_t));  
+// int main() {
+//     db_t *p = malloc(sizeof(db_t));  
 
-    printf("%p\n", p);
+//     printf("%p\n", p);
 
-    p->num = 100;
-    printf("p->ptr (garbage):  %p\n", p->ptr);
+//     p->num = 100;
 
-    p->ptr = NULL;
+//     printf("p->num = %u\n", p->num);
 
-    printf("p->ptr (after NULL): %p\n", p->ptr);
+//     free(p);
+//     return 0;
+// }
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
 
-    printf("p->num = %u\n", p->num);
+typedef struct node{
+    uint32_t data;
+    struct node *next;
+}node_t;
 
-    free(p);
-    return 0;
+// node_t *head = NULL;
+// node_t *tmp  = NULL;
+
+// node_t* create(){
+//     if (head == NULL)
+//     {
+//         head = (node_t*)calloc(1,sizeof(node_t));
+//         head = tmp;
+//     }
+//     else{
+
+//     }
+// }
+int main(){
+    node_t *head = NULL;
+    node_t *tmp1  = NULL;
+
+    if (head==NULL)
+    {
+        head=(node_t*)malloc(sizeof(node_t));
+        head->data = 0;
+        head->next = NULL;
+        tmp1 = head;
+    }
+    for (int i = 1; i < 11; i++)
+    {
+
+    node_t *node2 = malloc(sizeof(node_t));
+    node2->data = i;
+    node2->next = NULL;
+
+    tmp1->next = node2;
+
+    tmp1 = node2;
+
+    }
+    
+    tmp1=head;
+    while (tmp1->next!=NULL)
+    {
+        printf("-> %2u",tmp1->data);
+        tmp1=tmp1->next;
+    }
+    
 }
